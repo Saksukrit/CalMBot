04432e6b3ac97800f6dab8429d6e61f252b548e9
Saksukrit Ek-uru <kritzastep@gmail.com>
2017-02-21 12:17:06 +0700

    .

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-include ('line-bot-api/line-bot.php');
+include ('CalMBot/line-bot.php');
 $channelSecret = '7250dbd91d435551040866aed3c4b3ef';
 $access_token = 'uEaFS7lHeCcF0FEBVNQtuBTVpwVzjMCSebgBPdA/XUqgxzpYg8MHySfkmKpKys/TTEvQO99XihXnZaPKVO/4VsQXLqs8LQZdmskXuwncFHyI8/GZjv91J9Q/YN/pmATJTvlp6YOxOBypA2QFg1r6OwdB04t89/1O/w1cDnyilFU=';
 $bot = new BOT_API($channelSecret, $access_token);
