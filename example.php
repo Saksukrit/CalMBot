895ab68c1bd4daa0356925db6a40225655641c9f
Saksukrit Ek-uru <kritzastep@gmail.com>
2017-02-21 15:29:11 +0700

    .

--- example.php
+++ example.php
@@ -6,8 +6,8 @@ include ('line-bot-api/line-bot.php');
  * Variable
  * ==================================================================================== */
 
-$channelSecret = '047f......6294638e';
-$access_token  = 'iQmw+hHnV2bHs/4gsUsZRs...........1sMSsiaqwdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '7250dbd91d435551040866aed3c4b3ef';
+$access_token  = 'uEaFS7lHeCcF0FEBVNQtuBTVpwVzjMCSebgBPdA/XUqgxzpYg8MHySfkmKpKys/TTEvQO99XihXnZaPKVO/4VsQXLqs8LQZdmskXuwncFHyI8/GZjv91J9Q/YN/pmATJTvlp6YOxOBypA2QFg1r6OwdB04t89/1O/w1cDnyilFU=';
 
 $adminId       = 'U39f72c...........0d6ddf';
 
@@ -18,7 +18,7 @@ $adminId       = 'U39f72c...........0d6ddf';
 if (empty($_REQUEST['action'])) {
 
     $bot = new BOT_API($channelSecret, $access_token);
-	
+
     if (!empty($bot->isEvents)) {
 
         if (!empty($bot->isText)) {
@@ -34,7 +34,7 @@ if (empty($_REQUEST['action'])) {
             if ($bot->text == 'get_status:') {
                 $bot->replyMessageNew($bot->replyToken, '200: OK.');
             }
-			
+
             $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
 
         }
@@ -46,7 +46,7 @@ if (empty($_REQUEST['action'])) {
         if (!empty($bot->isSticker)) {
             $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
         }
-		
+
         $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
 
         if ($bot->isSuccess()) {
@@ -57,7 +57,7 @@ if (empty($_REQUEST['action'])) {
         }
 
         // Failed
-        echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+        echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody();
         exit();
 
     }
@@ -69,23 +69,23 @@ if (empty($_REQUEST['action'])) {
  * ==================================================================================== */
 
 else if (!empty($_REQUEST['action'])) {
-	
+
     $bot = new BOT_API($channelSecret, $access_token);
-	
+
     if ($_REQUEST['action'] == 'verify') {
 
         $bot->pushMessageToAdmin("\"SUCCESS\":\n" . json_encode(BOT_API::verify()));
         return;
 
     }
-	
+
     if ($_REQUEST['action'] == 'status') {
 
         $bot->pushMessageToAdmin('200: OK.');
         return;
 
     }
-	
+
     if ($bot->isSuccess()) {
 
         echo 'Succeeded!';
@@ -94,7 +94,7 @@ else if (!empty($_REQUEST['action'])) {
     }
 
     // Failed
-    echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+    echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody();
     exit();
 
 }
